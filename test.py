9a4e4b680832de49d1638a5f3fc006b39c1728a8
quan9395 <60518828+quan9395@users.noreply.github.com>
2023-11-02 17:54:40 +0700

    Update test.py

--- test.py
+++ test.py
@@ -49,37 +49,35 @@ if __name__ == '__main__':
     with torch.no_grad():
         for _, data in enumerate(tqdm(testloader)):
             images, labels = data
-            print(images[0])
-            exit(0)
-            label_birads = labels[0]
+            # label_birads = labels[0]
             label_density = labels[1]
-            label_birads = label_birads - 1
+            # label_birads = label_birads - 1
             label_density = label_density - 1
             images= images.to(device)
-            label_birads = label_birads.to(device)
+            # label_birads = label_birads.to(device)
             label_density = label_density.to(device)
-            _, birads, density = model(images)
+            density = model(images)
             # birads = torch.nn.functional.softmax(birads, dim=1)
             # density = torch.nn.functional.softmax(density, dim=1)
             # print(birads, density)
-            pred_birads = birads.max(1, keepdim=True)[1]
+            # pred_birads = birads.max(1, keepdim=True)[1]
             pred_density = density.max(1, keepdim=True)[1]
-            all_preds_birads.append(pred_birads)
+            # all_preds_birads.append(pred_birads)
             all_preds_density.append(pred_density)
-            all_labels_birads.append(label_birads)
+            # all_labels_birads.append(label_birads)
             all_labels_density.append(label_density)
             # print(pred_density, label_density)
             
 
-    all_preds_birads = torch.cat(all_preds_birads, axis=0)
+    # all_preds_birads = torch.cat(all_preds_birads, axis=0)
     all_preds_density = torch.cat(all_preds_density, axis=0)
-    all_labels_birads = torch.cat(all_labels_birads, axis=0)
+    # all_labels_birads = torch.cat(all_labels_birads, axis=0)
     all_labels_density = torch.cat(all_labels_density, axis=0)
-    all_preds_birads, all_preds_density, all_labels_birads, all_labels_density = all_preds_birads.cpu(), all_preds_density.cpu(), all_labels_birads.cpu(), all_labels_density.cpu()
+    all_preds_density, all_labels_density = all_preds_density.cpu(), all_labels_density.cpu()
 
-    print("birads1: ", (all_labels_birads == 0).sum().item())
-    print("birads2: ", (all_labels_birads == 1).sum().item())
-    print("birads3: ", (all_labels_birads == 2).sum().item())
+    # print("birads1: ", (all_labels_birads == 0).sum().item())
+    # print("birads2: ", (all_labels_birads == 1).sum().item())
+    # print("birads3: ", (all_labels_birads == 2).sum().item())
 
     print("densityA: ", (all_labels_density == 0).sum().item())
     print("densityB: ", (all_labels_density == 1).sum().item())
@@ -88,23 +86,23 @@ if __name__ == '__main__':
 
 
     # constant for classes
-    classes1 = ('BIRADS-1','BIRADS-2','BIRADS-3')
+    # classes1 = ('BIRADS-1','BIRADS-2','BIRADS-3')
     classes2 = ('Density A','Density B','Density C','Density D')
-    # Build confusion matrix
-    cf_matrix = confusion_matrix(all_labels_birads, all_preds_birads)
-    df_cm = pd.DataFrame(cf_matrix , index = [i for i in classes1],
-                        columns = [i for i in classes1])
-    with pd.option_context('display.max_columns', None):
-      print(df_cm)
-    # plt.figure(figsize = (12,7))
-    cmn = cf_matrix.astype('float') / cf_matrix.sum(axis=1)[:, np.newaxis]
-    sn.heatmap(cmn, annot=True, fmt='.2f', xticklabels=[i for i in classes1], yticklabels=[i for i in classes1])
-    plt.title('Confusion Matrix')
-    plt.ylabel('Actal Values')
-    plt.xlabel('Predicted Values')
-    plt.savefig(r"./birads_cfm.png")
+    # # Build confusion matrix
+    # cf_matrix = confusion_matrix(all_labels_birads, all_preds_birads)
+    # df_cm = pd.DataFrame(cf_matrix , index = [i for i in classes1],
+    #                     columns = [i for i in classes1])
+    # with pd.option_context('display.max_columns', None):
+    #   print(df_cm)
+    # # plt.figure(figsize = (12,7))
+    # cmn = cf_matrix.astype('float') / cf_matrix.sum(axis=1)[:, np.newaxis]
+    # sn.heatmap(cmn, annot=True, fmt='.2f', xticklabels=[i for i in classes1], yticklabels=[i for i in classes1])
+    # plt.title('Confusion Matrix')
+    # plt.ylabel('Actal Values')
+    # plt.xlabel('Predicted Values')
+    # plt.savefig(r"./birads_cfm.png")
     
-    plt.clf()
+    # plt.clf()
 
     cf_matrix2 = confusion_matrix(all_labels_density, all_preds_density)
     df_cm2 = pd.DataFrame(cf_matrix2 , index = [i for i in classes2],
